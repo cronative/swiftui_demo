dc038928a49e89b9f916370129f81259b951047b
Nikunj Munjiyasara <nikunj.appz@gmail.com>
2022-01-04 12:04:09 +0530

    initial code
    SwiftUI

---
+++ SwiftUI/SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI
+//
+//  Created by Apple on 16/02/21.
+//  Copyright © 2021 Nikunj M. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
